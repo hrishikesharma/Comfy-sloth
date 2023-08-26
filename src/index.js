b59096a90b8e3269c37d8e499a102888f5f0061c
Hrishikesh Sarma <hsarma414@gmail.com>
2023-08-27 00:27:21 +0530

    final project

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ProductsProvider } from './context/products_context';
+import { FilterProvider } from './context/filter_context';
+import { CartProvider } from './context/cart_context';
+import { UserProvider } from './context/user_context';
+import { Auth0Provider } from '@auth0/auth0-react';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Auth0Provider
+    domain="dev-qxdsfiqqud228je6.us.auth0.com"
+    clientId="5jRzHg1hzrUrUmJPM6U5XsTDFuE6UCOV"
+    authorizationParams={{
+      // redirect_uri: `${window.location.origin}/cart`,
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
+);
