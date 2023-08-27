dd3df5fcff00644383bc7709d40301330db347a3
Hrishikesh Sarma <hsarma414@gmail.com>
2023-08-27 13:22:01 +0530

    Added headings

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,28 +5,26 @@ import heroBcg from '../assets/hero-bcg.jpeg'
 import heroBcg2 from '../assets/hero-bcg-2.jpeg'
 const Hero = () => {
   return (
-    <Wrapper className='section-center'>
-      <article className='content'>
+    <Wrapper className="section-center">
+      <article className="content">
         <h1>
           design your <br />
           comfort zone
         </h1>
         <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, at
-          sed omnis corporis doloremque possimus velit! Repudiandae nisi odit,
-          aperiam odio ducimus, obcaecati libero et quia tempora excepturi quis
-          alias?
+          Shop from our online store in India for Furniture, Home Decor, Lamps,
+          Furnishings, Homeware and more at best prices.
         </p>
-        <Link to='/products' className='btn hero-btn'>
+        <Link to="/products" className="btn hero-btn">
           shop now
         </Link>
       </article>
-      <article className='img-container'>
-        <img src={heroBcg} alt='nice table' className='main-img' />
-        <img src={heroBcg2} alt='person working' className='accent-img' />
+      <article className="img-container">
+        <img src={heroBcg} alt="nice table" className="main-img" />
+        <img src={heroBcg2} alt="person working" className="accent-img" />
       </article>
     </Wrapper>
-  )
+  );
 }
 
 const Wrapper = styled.section`
